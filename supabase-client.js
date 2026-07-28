2ec42334ac439ba145d67efcde96bcbf507075a5
maedehkhabirii-spec <maedeh.khabirii@gmail.com>
2026-07-28 17:23:50 +0330

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-import { supabaseUrl, supabaseAnonKey } from './js/config.js';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
